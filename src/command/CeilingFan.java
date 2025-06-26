4eab8003da9693b03b34ce247c6baf6bf55c27d8
Ayodeji Bankole <bankymono@gmail.com>
2025-06-26 07:38:01 +0100

    #

--- src/command/CeilingFan.java
+++ src/command/CeilingFan.java
@@ -1,47 +1,45 @@
 package command;
 
 public class CeilingFan {
+
+    public static final int HIGH = 3;
+    public static final int MEDIUM = 2;
+    public static final int LOW = 1;
+    public static final int OFF = 0;
     String location = "";
-    int level;
-    public static final int HIGH = 2;
-    public static final int MEDIUM = 1;
-    public static final int LOW = 0;
+    int speed;
 
     public CeilingFan(String location) {
         this.location = location;
+        this.speed = OFF;
     }
 
     public void high() {
         // turns the ceiling fan on to high
-        level = HIGH;
+        speed = HIGH;
         System.out.println(location + " ceiling fan is on high");
-
     }
 
     public void medium() {
         // turns the ceiling fan on to medium
-        level = MEDIUM;
+        speed = MEDIUM;
         System.out.println(location + " ceiling fan is on medium");
     }
 
     public void low() {
         // turns the ceiling fan on to low
-        level = LOW;
+        speed = LOW;
         System.out.println(location + " ceiling fan is on low");
     }
 
     public void off() {
         // turns the ceiling fan off
-        level = 0;
+        speed = OFF;
         System.out.println(location + " ceiling fan is off");
     }
 
     public int getSpeed() {
-        return level;
-    }
-
-    public void on(){
-        System.out.println("CeilingFan is On!");
+        return speed;
     }
 }
 
